95e1415f80fc5534c4aabaf061d182a6b0b9b009
TwistedAsylumMC <twistedasylummc@gmail.com>
2022-10-01 20:56:24 +0100

    Loopback warning

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import Home from './Home';
 import Connection from "./Connection";
 
 import './style.css'
+import Loopback from "./Loopback";
 
 render(
     <div className={"flex flex-col justify-center px-12 text-slate-500 dark:text-slate-400 bg-white dark:bg-gray-900"}>
@@ -14,6 +15,7 @@ render(
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/connection" element={<Connection/>}/>
+                <Route path="/loopback" element={<Loopback/>}/>
             </Routes>
         </BrowserRouter>
     </div>,
