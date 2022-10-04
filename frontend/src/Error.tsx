c96db9d8e508846703660189d9e6261947f44cfa
TwistedAsylumMC <twistedasylummc@gmail.com>
2022-10-04 20:38:30 +0100

    Try again

--- frontend/src/Error.tsx
+++ frontend/src/Error.tsx
@@ -50,7 +50,7 @@ function Error() {
                     <button
                         onClick={() => navigate(`/?address=${params.get("address") || ""}&port=${params.get("port") || "19132"}`)}
                         className="text-white bg-slate-900 hover:bg-slate-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-700">
-                        Retry
+                        Try Again
                     </button>
                 </div>
             </div>
